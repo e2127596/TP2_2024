dca5ab177d675bd50864403b6fc0b8c7f76c3091
Jonathan Rivest <e1858237@cmaisonneuve.qc.ca>
2024-05-13 23:36:19 -0400

    Ajout fichier csv exercice 1

--- TP2/Exercice1.py
+++ TP2/Exercice1.py
@@ -1,1 +1,12 @@
 import json
+import csv
+
+def lirejson(fichierjson):
+    with open(fichierjson, 'r') as f:
+        contenu = json.load(f)
+        return contenu
+
+def ecrirecsv(contenujson, fichiercsv):
+    with open(fichiercsv, 'w') as csv:
+        csvwriter = csv.writer(csv, delimiter="\n")
+        csvwriter.writerow(["reel,imaginaire"])
