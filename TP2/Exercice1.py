6719a07250427bc82bb9dbbd6dde28b3bdae572c
Jonathan Rivest <e1858237@cmaisonneuve.qc.ca>
2024-05-14 00:13:43 -0400

    Quelques commentaires additionnels Exercice1

--- TP2/Exercice1.py
+++ TP2/Exercice1.py
@@ -4,14 +4,14 @@ import csv
 def lirejson(fichierjson):
     with open(fichierjson, 'r') as f:
         contenu = json.load(f)
-        return contenu
+        return contenu #Extraire le contenu du json pour la fonction qui suivra
 
 def ecrirecsv(contenujson, fichiercsv):
     with open(fichiercsv, 'w', newline="") as f:
-        csvwriter = csv.writer(f)
+        csvwriter = csv.writer(f) #Aucun delimiter spécifié, "," par défaut
         csvwriter.writerow(["reel","imaginaire"])
         for tuple in contenujson:
-            csvwriter.writerow(tuple)
+            csvwriter.writerow(tuple) #chaque tuple du json constitue une ligne du csv
 
 contenujson = lirejson("data.json")
 print(contenujson)
