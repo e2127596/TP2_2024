548d8e805152df1c6b226fc8959ebd58a4aea6a2
Hanif Abul <e2127596@cmaisonneuve.qc.ca>
2024-05-17 23:16:58 -0400

    Ajout des commentaires et quekque petit changement

--- TP2/Exercice2.py
+++ TP2/Exercice2.py
@@ -1,15 +1,15 @@
 import csv
-
+# Cette fonction permet de mettre les données de Pokémon du fichier csv dans un dictionnaire.
 def charger_pokémons_csv(csv_file):
-    pkmn_dict = {}
+    pokemon_dict = {} #On initialise le dictionnaire.
     with open(csv_file, 'r') as f:
         reader = csv.reader(f)
-        for row in reader:
+        for row in reader: 
             name = row[0]
             stats = list(map(int, row[1:]))
-            pkmn_dict[name] = stats
-    return pkmn_dict
-
+            pokemon_dict[name] = stats #le nom du Pokemon devient une clé dans le dictionnaire et les stats sont associe a cette clé.
+    return pokemon_dict
+#permet d'afficher les resultats de la commande dans le terminal.
 pkmn = charger_pokémons_csv('pokemon.csv')
 for nom, stats in pkmn.items():
     print(f"{nom}: {stats}")
